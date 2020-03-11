8548d3e7cb13a45202514d591ed694ac387b1dfb
Jacob Garcia <jagarcia1651@gmail.com>
2020-03-11 18:55:42 -0500

    introduced a strongly typed class for the workout model

--- src/models/workout.js
+++ src/models/workout.js
@@ -1,3 +1,22 @@
-// our example model is just an Array
-const facets = [];
+'use strict';
+
+class Work {
+  constructor (workData) {
+    this.id = workData.id;
+    this.date = workData.date;
+    this.weight = workData.weight;
+    this.exercise = workData.exercise;
+  }
+}
+
+exports.findById = ((id) => {
+  return new Promise ((resolve, reject) => {
+    let workData = `dynamo query`;
+    let cust = new Work (workData);
+    });
+  });
+});7
+
+
 export default facets;
+
